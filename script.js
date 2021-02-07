b1ef5c6943b442b2290e32104d316ca795ac9431
MostafaFathy367 <mostafa.fathy367@gmail.com>
2021-02-07 18:42:04 +0200

    Add Animation

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// var iconAnim = document.getElementById(ani);
+var iconAnim = document.querySelectorAll(".feature svg"),
+  featuCont = document.querySelectorAll(".feature-content");
 
-// window.onscroll = function () {
-//   if (window.scrollY >= 280){
-//     console.log(scrollY);
-//   }
-  
-
-// }
+window.onscroll = function () {
+  if (window.scrollY >= 300) {
+    iconAnim.forEach((anim) => {
+      anim.style.transform = "rotateX(0deg)";
+    })
+    featuCont.forEach((feat) => {
+      feat.style.transform = "scale(1)"
+    })
+  }
+}
